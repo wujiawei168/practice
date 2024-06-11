17ebd2c8d88cd4b02f79ab3c2d1ac197cacd8b5c
wujiawei168 <160474074+wujiawei168@users.noreply.github.com>
2024-06-11 14:31:37 +0800

    Update helloworld.cpp
    实现打印helloworld

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+#include <windows.h>
 
+int main() { //函数头
+	//函数体
+	using namespace std; //using 编译指令
+	cout << "Hello, World!" //输出“Hello,World!”
+		<< endl; //换行
+
+	Sleep(3000);
+	return 0; //返回语句，结束main()函数
+}
